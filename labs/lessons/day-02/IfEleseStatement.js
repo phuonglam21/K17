626ab22d07e616078a0e789b37a9889618432d05
Lam Nguyen <phuonglap21112000@gmail.com>
2025-07-23 02:08:54 +0700

    m

---
+++ labs/lessons/day-02/IfEleseStatement.js
@@ -0,0 +1,10 @@
+// Global block scope
+let isHeOnTime = false;
+
+if (isHeOnTime) {
+    //Block scope 1
+    console.log(`Let's talk`);
+} else {
+    //Block scope 2
+    console.log(`write a letter`);
+}
