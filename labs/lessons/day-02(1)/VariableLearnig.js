e936cf2a6b03d827086dc1003a1e88bdb3d48628
Lam Nguyen <phuonglap21112000@gmail.com>
2026-02-03 23:58:54 +0700

    comit bai tap

---
+++ labs/lessons/day-02(1)/VariableLearnig.js
@@ -0,0 +1,29 @@
+// camelcase
+
+let myName = "Leon"
+console.log(myName);
+
+// Re-assign: gán lại giá trị
+// Re-declare: khai báo lại biến
+
+myName = "Lâm";
+
+// Constant khai báo hằng số
+const PI_NUMBER = 3 / 14;
+
+
+// 1.7976931348623157e+308 -> 1.7976931348623157 * 10^308
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE)
+
+// check type of variable
+console.log(typeof (1.7976931348623157e+308));
+console.log(Infinity > 1.7976931348623157 * 10 ** 308);
+console.log(-2 / 0);
+
+//Boolen 1 loại dữ liệu mang trong mình 2 giá trị
+
+let amIRight = false;
+amIRight = "true"
+console.log(amIRight);
+
