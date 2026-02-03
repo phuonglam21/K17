dbdbdaa85abe0bf18fce55d52690eaf3991224d0
Lam Nguyen <phuonglap21112000@gmail.com>
2026-02-04 01:05:14 +0700

    comit

--- labs/lessons/day-02(1)/OperatorLearning.js
+++ labs/lessons/day-02(1)/OperatorLearning.js
@@ -1,27 +1,35 @@
 let myNum1 = 1;
 let myNum2 = 2;
 
-//1. Right operand and left operand
+// 1. Right openrand and left operand
 let result = myNum1 + myNum2;
-console.log('Total value' + result);
-console.log('Total value:', result);
-//String litteral /template: badsic 
+console.log('Toal value :' + result);
+console.log('Total value :', result);
 
+//String literal/template: backstick
 
 console.log(`5/2: ${5 / 2}`);
 console.log(`4/2: ${4 / 2}`);
-console.log(`10/4: ${10 / 3}`);
+console.log(`10/3: ${10 / 3}`)
 
-//Rounding nearest
-let roundingNearest = Math.round( 9 / 2);
-console.log(`9/2(Rounding nearest): ${roundingNearest}`);
+//Rounding nearest làm tròn gần 
+let roundingNerest = Math.round(10 / 3);
+console.log(` 10/3 ${roundingNerest}`);
 
-//Rounding floor
-let roundingFloor = Math.floor( 9 / 2);
-console.log(`9/2(Rounding floor): ${roundingFloor}`);
+//Rounding floor làm tròn lên 
+let roundingFloor = Math.floor(9 / 2);
+console.log(` 9/2 ${roundingFloor}`);
 
-//Rounding ceil
-let roundingCeil = Math.ceil( 9 / 2);
-console.log(`9/2(Rounding ceil): ${roundingCeil}`);
+// Rounding ceiling làm tròn xuống
+let roundingCeil = Math.ceil(10/3);
+console.log(` 10/3(Rounding ceiling ): ${roundingCeil}`);
 
-//to fixed number 
+// T fixed number 
+let anotherResult = 10 /3; 
+let MAX_FLOATING_NUMBER = 2;
+let toFixedNumber = anotherResult.toFixed(MAX_FLOATING_NUMBER);
+console.log(` 10/3 to fixed number : ${toFixedNumber}`);
+
+//Square  up a number
+let squareNumber = 9 ** 3;
+console.log(squareNumber);
---
+++ labs/lessons/day-02(1)/PrefixPostfix.js
@@ -0,0 +1,9 @@
+// ++ , --
+let a = 1;
+// Prefix
+let myNumber = ++a;
+console.log ()
+
+
+// Postfix
+// myNumber= a++;
