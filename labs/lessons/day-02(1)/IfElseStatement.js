e1356cad6ae2c185adc3f52bac1e31d8217e6ead
Lam Nguyen <phuonglap21112000@gmail.com>
2026-02-11 15:23:05 +0700

    update lessons except secret.js

--- labs/lessons/day-02(1)/IfElseStatement.js
+++ labs/lessons/day-02(1)/IfElseStatement.js
@@ -1,14 +1,34 @@
-// Global block scope
-let isHeOnTime = true
-let myName = "Tun";
+const readline = require ("readline-sync");
+
+let arrivalTime = readline.question("Arrival Time:");
+// Convert string into number
+let arrivalTimeNum = Number(arrivalTime);
+let isHeOnTime = (arrivalTimeNum) === 7; // "7"  ==="7" 
+
 if (isHeOnTime) {
-    //Block scope 1
     console.log("let's talk");
-    myName = "teo";
 } else {
-    //Block scope 2
     console.log("write a letter");
-    myName = "ti";
 }
 
-console.log(myName);
+
+//Toán tử 3 ngôi ternary operator 
+let msg = isHeOnTime?  "let's talk" : "write a letter";
+console.log(msg);
+
+
+
+
+
+
+
+
+
+
+
+//! true -> false 
+if (!isHeOnTime) {
+    console.log("let's talk");
+} else {
+    console.log("write a letter");
+}
